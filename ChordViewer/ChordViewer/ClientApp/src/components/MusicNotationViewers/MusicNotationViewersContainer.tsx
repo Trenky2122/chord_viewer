e6390f9d007abef45ad905435c1030aefa9b35ca
Peter Trencansky <peter@trencansky.com>
2022-04-02 08:12:45 +0200

    Music sheet viewer remade to use vex flow

--- ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicNotationViewersContainer.tsx
+++ ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicNotationViewersContainer.tsx
@@ -5,12 +5,12 @@ import MusicSheetViewer from "./MusicSheetViewer/MusicSheetViewer";
 const MusicNotationViewersContainer = ()=>{
     let viewers : IMusicNotationViewer[] = [];
     viewers.push(new MusicSheetViewer("canvas1"))
-    useEffect(()=>viewers.forEach(viewer => viewer.View("ABCDEFG")));
+    useEffect(()=>viewers.forEach(viewer => viewer.View("C#EbG#")));
     return (
         <div className={"container-fluid"}>
             <div className={"row"}>
                 <div className={"col"}>
-                    <canvas id={"canvas1"} width={"200"} height={"200"} />
+                    <canvas id={"canvas1"} width={420} height={200} />
                 </div>
             </div>
         </div>
--- ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/MusicSheetViewer.ts
+++ ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/MusicSheetViewer.ts
@@ -1,4 +1,5 @@
 import {IMusicNotationViewer} from "../IMusicNotationViewer";
+import {Accidental, Vex, Voice} from "vexflow";
 
 
 export class MusicSheetViewer implements IMusicNotationViewer{
@@ -30,23 +31,44 @@ export class MusicSheetViewer implements IMusicNotationViewer{
     }
 
     public View(toneKey: string): boolean {
-        // @ts-ignore
-        let canvas: HTMLCanvasElement = document.getElementById(this.canvasElementId);
-        if(!canvas)
-            return false;
-        let ctx = canvas.getContext("2d");
-        let width = canvas.offsetWidth;
-        let height = canvas.offsetHeight;
-        ctx!.strokeStyle = "#000000"
-        ctx!.fillStyle = "#000000"
-        ctx!.fillText("\uD834\uDD1E", width/4, height/2);
-        ctx!.clearRect(0, 0, width, height)
-        for(let i=0; i<5; i++){
-            ctx!.beginPath();
-            ctx!.moveTo(0, (i+1)*(height/7))
-            ctx!.lineTo(width, (i+1)*(height/7))
-            ctx!.stroke();
-        }
+        let VF = Vex.Flow;
+
+// We created an object to store the information about the workspace
+        var WorkspaceInformation = {
+            // The <canvas> element in which you're going to work
+            canvas: document.getElementById(this.canvasElementId) as HTMLCanvasElement,
+            // Vex creates a canvas with specific dimensions
+            canvasWidth: 100,
+            canvasHeight: 100
+        };
+
+// Create a renderer with Canvas
+        var renderer = new VF.Renderer(
+            WorkspaceInformation.canvas,
+            VF.Renderer.Backends.CANVAS
+        );
+
+// Expose the context of the renderer
+        var context = renderer.getContext();
+
+// And give some style to our SVG
+        context.setFont("Arial", 10, "").setBackgroundFillStyle("#eed");
+
+
+        /**
+         * Creating a new stave
+         */
+// Create a stave of width 400 at position x10, y40 on the SVG.
+        var stave = new VF.Stave(10, 40, 400);
+// Add a clef and time signature.
+        stave.addClef("treble").addTimeSignature("4/4");
+// Set the context of the stave our previous exposed context and execute the method draw !
+        stave.setContext(context).draw();
+
+
+        /**
+         * Draw notes
+         */
         let tones = [];
         for(let i=0; i<toneKey.length; i++){
             let begin = i;
@@ -57,18 +79,17 @@ export class MusicSheetViewer implements IMusicNotationViewer{
             }
             tones.push(toneKey.substring(begin, end));
         }
-        let takenPositions: number[] = [];
-        tones.forEach(tone => {
-            ctx!.beginPath();
-            let xcoordinate = width/2;
-            let position = this.notePositions[tone];
-            if(takenPositions.indexOf(position) != -1)
-                xcoordinate +=height/7;
-            else
-                takenPositions.push(position, position-1, position+1);
-            ctx!.arc(xcoordinate, height-(height/14)*(position), height/14, 0, 2*Math.PI)
-            ctx!.stroke();
-        })
+        let notes = [
+            new VF.StaveNote({clef: "treble", keys: tones.map(tone => tone.toLowerCase() + "/4"), duration: "q" }),
+
+        ];
+
+        var voice = new VF.Voice({num_beats: 1,  beat_value: 4}).setMode(Voice.Mode.SOFT);
+        voice.addTickables(notes);
+        Accidental.applyAccidentals([voice], "C");
+        var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+        voice.draw(context, stave);
         return true;
     }
 }
