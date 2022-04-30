893e146f639f5380331754815ad9702380430a74
Peter Trencansky <peter@trencansky.com>
2022-04-30 20:00:19 +0200

    Tabs editor part 2

--- ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/ContextMenu/TabsContextMenu.ts
+++ ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/ContextMenu/TabsContextMenu.ts
@@ -5,7 +5,7 @@ import ContextMenu from "./ContextMenu";
 class TabsContextMenu extends ContextMenu{
     public viewTab(x:number, y:number, parent: string, viewInEditor: boolean, moveFingerHere: boolean, removeThisString: boolean,
                    addLowerFretToView: boolean, addHigherFretToView: boolean, addString: boolean, removeString: boolean,
-                   changeStringTune: boolean){
+                   changeStringTune: boolean, resetEditor: boolean){
         this.currentX = x;
         this.currentY = y;
         let localization = new LocalizedStrings({
@@ -17,7 +17,8 @@ class TabsContextMenu extends ContextMenu{
                 addHigherFretToView: "Add higher fret to view",
                 addString: "Add string",
                 removeString: "Remove string",
-                changeStringTune: "Change string tune"
+                changeStringTune: "Change string tune",
+                resetEditor: "Reset editor"
             },
             sk: {
                 viewInEditor: "Zobraziť v editore",
@@ -27,14 +28,15 @@ class TabsContextMenu extends ContextMenu{
                 addHigherFretToView: "Pridať vyšší pražec do zobrazenia",
                 addString: "Pridať strunu",
                 removeString: "Odstrániť strunu",
-                changeStringTune: "Zmeniť ladenie struny"
+                changeStringTune: "Zmeniť ladenie struny",
+                resetEditor: "Restovať editor"
             }
         })
         let list = "<ul class='contextMenu'>";
         if(viewInEditor)
             list+=this.createListItem(parent, "viewInEditorButton", "viewInEditor", localization.viewInEditor);
         if(moveFingerHere)
-            list+=this.createListItem(parent, "moveFingerHereButton", "moveFingerHere", localization.moveFingerHere);
+            list+=this.createListItemWithNumberInput(parent, "moveFingerHereButton", "moveFingerHere", localization.moveFingerHere, 4, 1);
         if(removeThisString)
             list+=this.createListItem(parent, "removeThisStringButton", "removeThisString", localization.removeThisString);
         if(addLowerFretToView)
@@ -46,10 +48,30 @@ class TabsContextMenu extends ContextMenu{
         if(removeString)
             list+=this.createListItem(parent, "removeStringButton", "removeString", localization.removeString);
         if(changeStringTune)
-            list+=this.createListItem(parent, "changeStringTuneButton", "changeStringTune", localization.changeStringTune);
+            list+=this.createListItemWithDropdown(parent, "changeStringTuneButton", "changeStringTune",
+                localization.changeStringTune, [["C", "C"], ["C#", "C#"], ["D", "D"], ["D#", "D#"], ["E", "E"],
+                    ["F", "F"], ["F#", "F#"], ["G", "G"], ["G#", "G#"], ["A", "A"], ["A#", "A#"], ["B", "B"]]);
+        if(resetEditor)
+            list+=this.createListItem(parent, "resetEditorButton", "resetEditor", localization.resetEditor);
         list+="</ul>";
         this.el.innerHTML = list;
         this.show(x, y);
+    }
+
+    createListItemWithNumberInput(parent: string, id: string, event: string, text: string, maxValue: number, minValue: number): string {
+        return "<li id='"+id+"' onclick='document.getElementById(\""+parent+"\")" +
+            ".dispatchEvent(new CustomEvent(\""+event+"\", {detail: document.getElementById(\"input"+id+"\").value}));' class='contextMenuItem'>"
+            +text+" <input type='number' id='input"+id+"' size='1' min='"+minValue.toString()+"' " +
+            "max='"+maxValue+"'></li>";
+    }
+
+    createListItemWithDropdown(parent: string, id: string, event: string, text: string, options: [string, string][]): string {
+        let retval = "<li id='"+id+"' onclick='document.getElementById(\""+parent+"\")" +
+            ".dispatchEvent(new CustomEvent(\""+event+"\",  {detail: document.getElementById(\"input"+id+"\").value}));' class='contextMenuItem'>"+text;
+        retval += " <select name='select' id='input"+id+"'>"
+        options.forEach(opt => retval+="<option value='"+opt[0]+"'>"+opt[1]+"</option>")
+        retval+="</select></li>";
+        return retval;
     }
 }
 export default TabsContextMenu;
--- ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/TabsViewer.ts
+++ ChordViewer/ChordViewer/ClientApp/src/components/MusicNotationViewers/MusicSheetViewer/TabsViewer.ts
@@ -5,7 +5,16 @@ import TabsContextMenu from "./ContextMenu/TabsContextMenu";
 
 export class TabsViewer implements IMusicNotationViewer{
     RepresentativeElement: HTMLDivElement;
-
+    tabInEditor: Tab = {id: 0, tabBarre:[], tabStrings: [], stringCount: 6, authorId: 0, toneKey:""};
+    editorContext?: CanvasRenderingContext2D;
+    editorMinfret: number=25;
+    editorMaxfret: number=0;
+    width = 400;
+    height = 120;
+    xDelta = 15;
+    yDelta = 15;
+    lastClickedString = 0;
+    lastClickedFret = 0;
     constructor(private parentDivId: string, private contextMenu: TabsContextMenu) {
         this.RepresentativeElement = document.getElementById(parentDivId) as HTMLDivElement;
     }
@@ -32,7 +41,7 @@ export class TabsViewer implements IMusicNotationViewer{
         colEditor.className = "col";
         mainRow.appendChild(colEditor);
         colEditor.innerHTML="<p>Tab editor</p>"
-        colEditor.appendChild(this.createEditor(6));
+        colEditor.appendChild(this.createEditor());
         let containerTabs = document.createElement("div");
         containerTabs.className = "container-fluid";
         colTabs.appendChild(containerTabs);
@@ -47,37 +56,59 @@ export class TabsViewer implements IMusicNotationViewer{
         });
     }
 
-    createEditor(strings: number): HTMLCanvasElement{
+    createEditor(): HTMLCanvasElement{
         let canvas = document.createElement("canvas");
-
-        return this.createTabCanvas({id: 0, tabBarre:[], tabStrings: [], stringCount: strings, authorId: 0, toneKey:""} , canvas);
+        this.editorContext = canvas.getContext("2d")!;
+        canvas.id = "editor_canvas";
+        canvas.addEventListener("addString", ()=>this.addString());
+        canvas.addEventListener("removeString", ()=>this.removeString());
+        canvas.addEventListener("addHigherFretToView", ()=>this.addHigherFret());
+        canvas.addEventListener("addLowerFretToView", ()=>this.addLowerFret());
+        canvas.addEventListener("resetEditor", ()=>this.resetEditor());
+        canvas.addEventListener("changeStringTune", (ev: any)=>this.changeStringTune(ev.detail));
+        return this.createTabCanvas(this.tabInEditor , canvas);
     }
 
     createTabCanvas(tab: Tab, canvas? :HTMLCanvasElement): HTMLCanvasElement{
-        if(!canvas)
+        let isEditor = true;
+        if(!canvas) {
             canvas = document.createElement("canvas");
-        canvas.addEventListener('contextmenu', (e: MouseEvent) => this.handleRightClick(e,"ll"), false);
+            canvas.id = tab.id.toString()+"_tab";
+            isEditor = false;
+            canvas.addEventListener('viewInEditor', ()=>this.setTabToEditor(tab));
+        }
+        canvas.addEventListener('contextmenu', (e: MouseEvent) => this.handleRightClick(e, canvas!.id), false);
         canvas.addEventListener('click', this.handleClick.bind(this), false);
-        let width = 400;
-        let height = 120;
-        let xDelta = 15;
-        let yDelta = 15;
-        canvas.width = width + 2*xDelta;
-        canvas.height = height + 2*yDelta;
+        canvas.width = this.width + 2*this.xDelta;
+        canvas.height = this.height + 2*this.yDelta;
         let ctx = canvas.getContext("2d");
-        ctx!.strokeStyle = "#000000";
-        ctx!.fillStyle = "#FF9999";
-        ctx!.scale(1, 1);
+        this.redrawTabOnCanvas(ctx!, tab, isEditor);
+        return canvas;
+    }
+
+    setTabToEditor(tab: Tab){
+        this.tabInEditor = tab;
+        this.editorMinfret = 25;
+        this.editorMaxfret = 0;
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
+
+    redrawTabOnCanvas(ctx: CanvasRenderingContext2D, tab: Tab, isEditor: boolean){
+        ctx.clearRect(0, 0, 500, 500);
+        ctx.strokeStyle = "#000000";
+        ctx.fillStyle = "#FF9999";
+        ctx.scale(1, 1);
         for(let i=0; i<tab.stringCount; i++){
             let string = tab.tabStrings.filter(s => s.stringOrder === i);
             let stringText = "";
             if(string.length > 0)
                 stringText = string[0].tune;
-            ctx!.beginPath();
-            ctx!.strokeText(stringText, 0, i*height/(tab.stringCount-1) + yDelta);
-            ctx!.moveTo(xDelta, i*height/(tab.stringCount-1) + yDelta);
-            ctx!.lineTo(width + xDelta, i*height/(tab.stringCount-1) + yDelta);
-            ctx!.stroke();
+            ctx.beginPath();
+            ctx.strokeText(stringText, 0, i*this.height/(tab.stringCount-1) + this.yDelta);
+            ctx.moveTo(this.xDelta, i*this.height/(tab.stringCount-1) + this.yDelta);
+            ctx.lineTo(this.width + this.xDelta, i*this.height/(tab.stringCount-1) + this.yDelta);
+            ctx.stroke();
         }
         let minFret = Math.min(...tab.tabStrings.map(s => s.fret));
         let maxFret = Math.max(...tab.tabStrings.map(s => s.fret));
@@ -87,13 +118,18 @@ export class TabsViewer implements IMusicNotationViewer{
             minFret = 0;
             maxFret = 5;
         }
+        if(isEditor){
+            minFret = Math.min(minFret, this.editorMinfret);
+            maxFret = Math.max(maxFret, this.editorMaxfret);
+            this.editorMinfret = minFret;
+            this.editorMaxfret = maxFret;
+        }
         for (let i = 0; i<=maxFret-minFret; i++){
-            ctx!.beginPath();
-            ctx!.strokeText((i+minFret).toString(), i*width/(maxFret-minFret) + xDelta, yDelta/2)
-            ctx!.moveTo(i*width/(maxFret-minFret) + xDelta, yDelta)
-            ctx!.lineTo(i*width/(maxFret-minFret) + xDelta, height + yDelta)
-            ctx!.stroke();
-            console.log(i*width/(maxFret-minFret))
+            ctx.beginPath();
+            ctx.strokeText((i+minFret).toString(), i*this.width/(maxFret-minFret) + this.xDelta, this.yDelta/2);
+            ctx.moveTo(i*this.width/(maxFret-minFret) + this.xDelta, this.yDelta);
+            ctx.lineTo(i*this.width/(maxFret-minFret) + this.xDelta, this.height + this.yDelta);
+            ctx.stroke();
         }
 
         let radius = 10;
@@ -102,42 +138,41 @@ export class TabsViewer implements IMusicNotationViewer{
             let strings = tab.tabStrings.filter(s => s.stringOrder === i);
             if(strings.length > 0) {
                 let ts = strings[0];
-                ctx!.beginPath();
-                ctx!.arc((ts.fret - minFret)* width / (maxFret - minFret) - width / (2 * (maxFret - minFret)) + xDelta,
-                    i * height / (tab.stringCount - 1) + yDelta, radius, 0, Math.PI * 2);
-                ctx!.fill();
-                ctx!.beginPath();
-                ctx!.strokeText(ts.suggestedFinger.toString(), (ts.fret - minFret) * width / (maxFret - minFret) - width / (2 * (maxFret - minFret)) + xDelta - 3,
-                    i * height / (tab.stringCount - 1) + yDelta + 3);
-                ctx!.stroke();
+                ctx.beginPath();
+                ctx.arc((ts.fret - minFret - 0.5)* this.width / (maxFret - minFret) + this.xDelta,
+                    i * this.height / (tab.stringCount - 1) + this.yDelta, radius, 0, Math.PI * 2);
+                ctx.fill();
+                ctx.beginPath();
+                ctx.strokeText(ts.suggestedFinger.toString(),
+                    (ts.fret - minFret) * this.width / (maxFret - minFret) - this.width / (2 * (maxFret - minFret)) + this.xDelta - 3,
+                    i * this.height / (tab.stringCount - 1) + this.yDelta + 3);
+                ctx.stroke();
             }
             else {
-                ctx!.beginPath();
-                ctx!.strokeText("x",  xDelta - 3,
-                    i * height / (tab.stringCount - 1) + yDelta + 3);
-                ctx!.stroke();
+                ctx.beginPath();
+                ctx.strokeText("x",  this.xDelta - 3,
+                    i * this.height / (tab.stringCount - 1) + this.yDelta + 3);
+                ctx.stroke();
             }
         }
 
         tab.tabBarre.forEach(b => {
-           for(let i= 0; i<10; i++){
-               ctx!.beginPath();
-               ctx!.moveTo((b.fret - minFret) * width / (maxFret - minFret) - width / (2 * (maxFret - minFret)) + xDelta - radius,
-                   b.stringBegin * height / (tab.stringCount - 1) + yDelta);
-               ctx!.bezierCurveTo((b.fret - minFret) * width / (maxFret - minFret) - width / (2 * (maxFret - minFret)) + xDelta - radius,
-                   b.stringBegin * height / (tab.stringCount - 1) + yDelta,
-                   (b.fret-1 - minFret) * width / (maxFret - minFret) + xDelta + i - radius,
-                   ((b.stringBegin+b.stringEnd)/2) * height / (tab.stringCount - 1) + yDelta,
-                   (b.fret - minFret) * width / (maxFret - minFret) - width / (2 * (maxFret - minFret)) + xDelta - radius,
-                   b.stringEnd * height / (tab.stringCount - 1) + yDelta);
-               ctx!.stroke();
-           }
-           ctx!.strokeText(b.suggestedFinger.toString(), (b.fret-1 - minFret) * width / (maxFret - minFret) + xDelta + 2,
-                ((b.stringBegin+b.stringEnd)/2) * height / (tab.stringCount - 1) + yDelta);
-           ctx!.stroke();
+            for(let i= 0; i<10; i++){
+                ctx.beginPath();
+                ctx.moveTo((b.fret - minFret) * this.width / (maxFret - minFret) - this.width / (2 * (maxFret - minFret)) + this.xDelta - radius,
+                    b.stringBegin * this.height / (tab.stringCount - 1) + this.yDelta);
+                ctx.bezierCurveTo((b.fret - minFret) * this.width / (maxFret - minFret) - this.width / (2 * (maxFret - minFret)) + this.xDelta - radius,
+                    b.stringBegin * this.height / (tab.stringCount - 1) + this.yDelta,
+                    (b.fret-1 - minFret) * this.width / (maxFret - minFret) + this.xDelta + i - radius,
+                    ((b.stringBegin+b.stringEnd)/2) * this.height / (tab.stringCount - 1) + this.yDelta,
+                    (b.fret - minFret) * this.width / (maxFret - minFret) - this.width / (2 * (maxFret - minFret)) + this.xDelta - radius,
+                    b.stringEnd * this.height / (tab.stringCount - 1) + this.yDelta);
+                ctx.stroke();
+            }
+            ctx.strokeText(b.suggestedFinger.toString(), (b.fret-1 - minFret) * this.width / (maxFret - minFret) + this.xDelta + 2,
+                ((b.stringBegin+b.stringEnd)/2) * this.height / (tab.stringCount - 1) + this.yDelta);
+            ctx.stroke();
         });
-        console.log(tab);
-        return canvas;
     }
 
     getActualToneKey(): string {
@@ -146,8 +181,24 @@ export class TabsViewer implements IMusicNotationViewer{
 
     handleRightClick(e: MouseEvent, targetId: string){
         e!.preventDefault();
-        this.contextMenu.viewTab(e.pageX, e.pageY, targetId, true, true,
-            true, true, true, true, true, true);
+        this.lastClickedString=this.getEditorStringFromOffsetPosition(e.offsetY);
+        this.lastClickedFret=this.getEditorFretFromOffsetPosition(e.offsetX);
+        this.contextMenu.viewTab(e.pageX, e.pageY, targetId,
+            targetId!=="editor_canvas", targetId==="editor_canvas",
+            targetId==="editor_canvas", targetId==="editor_canvas",
+            targetId==="editor_canvas", targetId==="editor_canvas",
+            targetId==="editor_canvas", targetId==="editor_canvas",
+            targetId==="editor_canvas");
+    }
+
+    getEditorStringFromOffsetPosition(y: number){
+        let string = (this.tabInEditor.stringCount -1)*(y - this.yDelta)/this.height;
+        return Math.round(string);
+    }
+
+    getEditorFretFromOffsetPosition(x: number){
+        let fret = (x - this.xDelta)*(this.editorMaxfret - this.editorMinfret)/this.width + this.editorMinfret + 0.5;
+        return Math.round(fret);
     }
 
     handleClick(e: MouseEvent){
@@ -155,5 +206,41 @@ export class TabsViewer implements IMusicNotationViewer{
         this.contextMenu.hide();
     }
 
+    addString(){
+        this.tabInEditor.stringCount+=1;
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
 
+    removeString(){
+        this.tabInEditor.stringCount-=1;
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
+
+    addHigherFret(){
+        this.editorMaxfret+=1;
+        console.log(this.editorMaxfret);
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
+
+    addLowerFret(){
+        this.editorMinfret+=1;
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
+
+    resetEditor(){
+        this.tabInEditor = {id: 0, tabBarre:[], tabStrings: [], stringCount: 6, authorId: 0, toneKey:""};
+        this.editorMinfret = 25;
+        this.editorMaxfret = 0;
+        this.redrawTabOnCanvas(this.editorContext!, this.tabInEditor, true);
+        this.contextMenu.hide();
+    }
+
+    changeStringTune(tune: string){
+        console.log(tune);
+        let stringIndex = this.tabInEditor.tabStrings.findIndex(x => x.stringOrder === this.lastClickedString);
+    }
 }
